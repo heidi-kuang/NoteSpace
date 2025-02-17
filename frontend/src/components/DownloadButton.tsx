7004ecbbd6e92c481ab6ddd4577812f3627e7fe1
Heidi Kuang <kuangheidi27@gmail.com>
2025-02-17 00:53:23 -0600

    added tooltip to download button for filename

--- frontend/src/components/DownloadButton.tsx
+++ frontend/src/components/DownloadButton.tsx
@@ -1,4 +1,10 @@
 import { Button } from '@/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 interface DownloadButtonProps {
   downloadLink: string | null;
@@ -7,16 +13,30 @@ interface DownloadButtonProps {
 
 const DownloadButton: React.FC<DownloadButtonProps> = ({ downloadLink, processedFileName }) => {
   if (!downloadLink) return null;
+  
+  const blurb = processedFileName;
+  
   return (
     <div className="mt-4">
-      <a href={downloadLink} download={processedFileName}>
-        <Button variant="outline">
-          Download
-        </Button>
-      </a>
+      <TooltipProvider delayDuration={0}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <a href={downloadLink} download={processedFileName}>
+              <Button variant="outline">
+                Download
+              </Button>
+            </a>
+          </TooltipTrigger>
+          <TooltipContent side="bottom" sideOffset={16}>
+            <p className="text-gray-600 text-sm">{blurb}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      
     </div>
     
   )
 }
 
 export default DownloadButton
+
